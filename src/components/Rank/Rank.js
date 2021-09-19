a2b58b04f8556741cc7699320650d10873af9230
Angelo <angelo.adduci1991@gmail.com>
2021-09-19 19:42:26 +0200

    Add user rank

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Rank = () => {
+    return (
+        <div  className="flex flex-column justify-center items-center mv2 w-100">
+            <p className="mb0 f3 white">
+                {'User your current rank is:'}
+            </p>
+            <p className="bg-animate white hover-bg-moon-gray hover-black f3 ba br-100 pa2">
+                {'#5'}
+            </p>
+        </div>
+    )
+}
+
+export default Rank
