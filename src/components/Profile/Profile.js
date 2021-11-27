f004b0da79a14bb84848e56e8839daad2a7be001
Angelo <angelo.adduci1991@gmail.com>
2021-11-27 17:27:51 +0100

    Add profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,46 @@
+import React, {useEffect} from "react";
+
+const Profile = ({ id, loadUserData, onRouteChange, user }) => {
+    useEffect( () => {
+        fetch('http://localhost:3000/profile/' + id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then( response => response.json())
+        .then( data =>{
+            if(data.id){
+                loadUserData({
+                    id: data.id,
+                    username: data.username,
+                    email: data.email,
+                    password: data.password,
+                    rank: data.rank,
+                    created_at: data.created_at,
+                })
+            }  else{
+                onRouteChange('/')
+            }
+        })
+    }, [loadUserData, onRouteChange, id])
+    
+    return (
+        <div  className="flex flex-column justify-center items-center mv2 w-100">
+            <p className="mb0 f3 white">
+                {user.username}
+            </p>
+            <p className="mb0 f3 white">
+                {user.email}
+            </p>
+            <p className="mb0 f3 white">
+                {user.created_at}
+            </p>
+            <p className="bg-animate white hover-bg-moon-gray hover-black f3 ba br-100 pa2">
+                {'#' + user.rank}
+            </p>
+        </div>
+    )
+}
+
+export default Profile
