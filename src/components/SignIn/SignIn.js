5d3ca2148ddc21a4bec424242d57b8d7f9f8816f
Angelo <angelo.adduci1991@gmail.com>
2021-11-27 17:28:47 +0100

    Add signin component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,25 +1,72 @@
-import React from "react";
-import './SignIn.css'
+import React, { useState } from 'react'
 
+const SignIn = ({ onRouteChange, loadUserData }) => {
 
-const SignIn = ({ onRouteChange }) => {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ loginError, setLoginError ] = useState('')
+
+    const onSubmitSignIn = () => {
+        fetch('http://localhost:3000/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        .then( response => response.json())
+        .then( data =>{
+            if(data.success === 'success'){
+                loadUserData({
+                    id: data.id,
+                    username: data.username,
+                    email: data.email,
+                    rank: data.rank,
+                    created_at: data.created_at,
+                })
+                onRouteChange('/')
+            }  else{
+                onLoginError(data)
+            }
+        })
+    }
+
+    const onEmailChange = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const onPasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const onLoginError = (error) => setLoginError(error)
+
     return (
-        <article className="mw6 center br3 pa4 pa5-ns mv3 ba b--black-10 shadow-5">
+        <article className="mw6 center br3 pa4 pa4-ns mv3 ba b--black-10 shadow-5">
             <div className="measure center">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                 <legend className="f2 center fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3 ph4">
                     <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                    <input onChange={onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
                 </div>
                 <div className="mv3 ph4">
                     <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                    <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                    <input onChange={onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                {
+                    loginError === 'login error'
+                        ? <div className="fw5 mt2 red f5">{loginError}</div>
+                        : null
+                }    
+                    
                 </div>
                 </fieldset>
                 <div className="w-100 flex justify-center items-center">
                     <button 
-                        onClick={() => onRouteChange('/')}
+                        onClick={onSubmitSignIn}
                         className="center b ph3 pv2 button-reset ba b--black bg-transparent grow pointer f5 dib"
                         type="submit"
                     >
