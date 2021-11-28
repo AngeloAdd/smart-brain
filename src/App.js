48470b9c8a4ff77aeb2feb7c486098756f119862
Angelo <angelo.adduci1991@gmail.com>
2021-11-29 00:05:09 +0100

    Run eslint on App and index

--- src/App.js
+++ src/App.js
@@ -3,142 +3,142 @@ import Navbar from './components/Navbar/Navbar.js'
 import SearchBar from './components/SearchBar/SearchBar.js'
 import Image from './components/Image/Image.js'
 import Rank from './components/Rank/Rank.js'
-import Particles from "react-tsparticles";
+import Particles from 'react-tsparticles'
 import particlesOption from './particles.json'
-import SignIn from './components/SignIn/SignIn';
-import React from 'react';
-import Register from './components/Register/Register';
-import clarifai from './var/clarifaiApi';
+import SignIn from './components/SignIn/SignIn'
+import React from 'react'
+import Register from './components/Register/Register'
+import clarifai from './var/clarifaiApi'
 import Profile from './components/Profile/Profile.js'
 
 const App = () => {
-
-  const [ input, setInput ] = useState('')
-  const [ imageUrl, setImageUrl ] = useState('')
-  const [ boxes, setBoxes ] = useState([])
-  const [ route, setRoute ] = useState('signin')
-  const [ isSignedIn, setIsSignedIn ] = useState(false)
-  const [ user , setUser] = useState({
-    id: 0,
-    username: '',
-    email:'',
-    rank:0,
-    created_at:'',
-    updated_at:'',
-  })
-
-  const loadUserData = (user) => {
-    setUser(obj => {
-      obj.id = user.id
-      obj.username = user.username
-      obj.email = user.email
-      obj.rank = user.rank
-      obj.created_at = user.created_at
-      obj.updated_at = user.updated_at
-      return obj
-    })
-  }
-
-  const onInputChange = (event) => {
-    setInput(event.target.value)
-  }
-
-  const calculateCoordinates = (data) => {
-    const coordinates = data.outputs[0].data.regions.map(el => {
-      return {
-        height: `${(el.region_info.bounding_box.bottom_row - el.region_info.bounding_box.top_row) * 100}%`,
-        width: `${(el.region_info.bounding_box.right_col - el.region_info.bounding_box.left_col) * 100}%`,
-        from_top: `${el.region_info.bounding_box.top_row * 100}%`,
-        from_left: `${el.region_info.bounding_box.left_col * 100}%`
-        }
-      })
-    return coordinates
-  }
-
-  const onButtonSubmit = () => {
-    clarifai.models.predict('f76196b43bbd45c99b4f3cd8e8b40a8a', input, {video: false})
-      .then(setImageUrl(input))
-      .then(resp => {
-        if(resp) {
-          fetch('http://localhost:3000/image',{
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              id: user.id,
-            })
-          })  
-          .then( resp => resp.json())
-          .then( data => {
-            if(data.rank){
-              console.log(data)
-              setUser({...user, rank: parseInt(data.rank)})
-            }
-          })
-        }
-        const coordinates = calculateCoordinates(resp)
-        setBoxes(coordinates)
-      })
-      .catch(e => console.log(e));
-  }
-
-  const clearInputBar = () => {
-    setInput('')
-  }
-
-  const clearImage = () => {
-    setImageUrl('')
-  }
-
-  const onRouteChange = (route) => {
-    if(route === 'signout'){
-      setRoute('signin')
-      setUser({
+    const [ input, setInput ] = useState('')
+    const [ imageUrl, setImageUrl ] = useState('')
+    const [ boxes, setBoxes ] = useState([])
+    const [ route, setRoute ] = useState('signin')
+    const [ isSignedIn, setIsSignedIn ] = useState(false)
+    const [ user, setUser] = useState({
         id: 0,
         username: '',
         email:'',
         rank:0,
         created_at:'',
         updated_at:'',
-      })
-      setIsSignedIn(false)
-      return 
-    } else if( route === '/'){
-      if(user.id !== 0){
-        setIsSignedIn(true)
-      } else{
-        setRoute('signin')
-        setIsSignedIn(false)
-        return
-      }
-    }
-    setRoute(route)
-  }
+    })
 
-  return (
-    <div>
-      <Particles
-           id="tsparticles"
-           options={ particlesOption }
-      />
-      <Navbar isSignedIn={isSignedIn} onRouteChange={onRouteChange} id={user.id} />
-      { route === '/'
-          ? <React.Fragment>
-              <Rank user={user}/>
-              <SearchBar input={input} clearInputBar={clearInputBar} onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
-              <Image clearInputBar={clearInputBar} clearImage={clearImage} boxes={boxes} imageUrl={imageUrl}/>
-            </React.Fragment>
-          : ( route === 'signin'
-              ? <SignIn onRouteChange={onRouteChange} loadUserData={loadUserData} />
-              : (route === 'profile/' + user.id 
-                  ? <Profile loadUserData={loadUserData} id={user.id} user={user} onRouteChange={onRouteChange}/>
-                  : <Register onRouteChange={onRouteChange} loadUserData={loadUserData}/>
+    const loadUserData = (user) => {
+        setUser(obj => {
+            obj.id = user.id
+            obj.username = user.username
+            obj.email = user.email
+            obj.rank = user.rank
+            obj.created_at = user.created_at
+            obj.updated_at = user.updated_at
+            return obj
+        })
+    }
+
+    const onInputChange = (event) => {
+        setInput(event.target.value)
+    }
+
+    const calculateCoordinates = (data) => {
+        const coordinates = data.outputs[0].data.regions.map(el => {
+            return {
+                height: `${(el.region_info.bounding_box.bottom_row - el.region_info.bounding_box.top_row) * 100}%`,
+                width: `${(el.region_info.bounding_box.right_col - el.region_info.bounding_box.left_col) * 100}%`,
+                from_top: `${el.region_info.bounding_box.top_row * 100}%`,
+                from_left: `${el.region_info.bounding_box.left_col * 100}%`,
+            }
+        })
+        return coordinates
+    }
+
+    const onButtonSubmit = () => {
+        clarifai.models.predict('f76196b43bbd45c99b4f3cd8e8b40a8a', input, {video: false})
+      .then(setImageUrl(input))
+      .then(resp => {
+          if(resp) {
+              fetch('http://localhost:3000/image', {
+                  method: 'PUT',
+                  headers: {
+                      'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                      id: user.id,
+                  }),
+              })  
+          .then( resp => resp.json())
+          .then( data => {
+              if(data.rank){
+                  console.log(data)
+                  setUser({...user,
+                      rank: parseInt(data.rank)})
+              }
+          })
+          }
+          const coordinates = calculateCoordinates(resp)
+          setBoxes(coordinates)
+      })
+      .catch(e => console.log(e))
+    }
+
+    const clearInputBar = () => {
+        setInput('')
+    }
+
+    const clearImage = () => {
+        setImageUrl('')
+    }
+
+    const onRouteChange = (route) => {
+        if(route === 'signout'){
+            setRoute('signin')
+            setUser({
+                id: 0,
+                username: '',
+                email:'',
+                rank:0,
+                created_at:'',
+                updated_at:'',
+            })
+            setIsSignedIn(false)
+            return 
+        } else if( route === '/'){
+            if(user.id !== 0){
+                setIsSignedIn(true)
+            } else{
+                setRoute('signin')
+                setIsSignedIn(false)
+                return
+            }
+        }
+        setRoute(route)
+    }
+
+    return (
+        <div>
+            <Particles
+                id="tsparticles"
+                options={ particlesOption }
+            />
+            <Navbar isSignedIn={isSignedIn} onRouteChange={onRouteChange} id={user.id} />
+            { route === '/'
+                ? <React.Fragment>
+                    <Rank user={user}/>
+                    <SearchBar input={input} clearInputBar={clearInputBar} onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
+                    <Image clearInputBar={clearInputBar} clearImage={clearImage} boxes={boxes} imageUrl={imageUrl}/>
+                </React.Fragment>
+                : ( route === 'signin'
+                    ? <SignIn onRouteChange={onRouteChange} loadUserData={loadUserData} />
+                    : (route === 'profile/' + user.id 
+                        ? <Profile loadUserData={loadUserData} id={user.id} user={user} onRouteChange={onRouteChange}/>
+                        : <Register onRouteChange={onRouteChange} loadUserData={loadUserData}/>
+                    )
                 )
-            )
-      }
-    </div>
-  );
+            }
+        </div>
+    )
 }
 
-export default App;
+export default App
