c7bbfa0324ab12acb2007fab733594de6bd838a4
Angelo <angelo.adduci1991@gmail.com>
2021-11-28 02:30:03 +0100

    Add link to home on logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactParallaxTilt from "react-parallax-tilt";
 import brain from './brain.png'
 
-const Logo = () => {
+const Logo = ({onRouteChange}) => {
     return (
         <ReactParallaxTilt options={{ tiltMaxAngleX: -55, tiltMaxAngleY: -55 }} className="w-10 pa2 br2 shadow-3 bg-logo">
-                <img src={brain} alt="brain"/>       
+                <img className="pointer" onClick={() => onRouteChange('/')} src={brain} alt="brain"/>       
         </ReactParallaxTilt>
     )
 }
